8219e9e01ba33d85b842fffb8d03fdbcce9c8600
temen-eng <temen.engineering@gmail.com>
2026-03-18 20:33:10 +0100

    Solve ninth exercise in Foundations

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let sum = 0;
+    if ((typeof(first) === "number" && first >= 0 && (first % 1 === 0)) && (typeof(second) === "number" && second >= 0 && (second % 1 === 0))) {
+        if (first > second) {
+            let third = first;
+            first = second;
+            second = third;
+        } 
+        for (let i = first; i <= second; i++){
+            sum = sum + i;
+        }
+        return sum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
