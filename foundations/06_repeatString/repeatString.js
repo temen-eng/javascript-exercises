dfca25cd0e851aaceedb5b83613a795302465a1d
temen-eng <temen.engineering@gmail.com>
2026-03-18 18:47:26 +0100

    Solve sixth exercise for JavaScript

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
+    additionText = '';
+    if (num < 0) {
+        return additionText = "ERROR";
+    } else {
+        for (i = 0; i < num; i++) {
+            additionText = additionText + text;
+        }
+        return additionText;
+    }
 
 };
+
+repeatString('hey', 3) // returns 'heyheyhey'
 
 // Do not edit below this line
 module.exports = repeatString;
