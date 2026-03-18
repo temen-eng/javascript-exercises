9080114d72ef17ba0eb0680d63be7e183adb192f
temen-eng <temen.engineering@gmail.com>
2026-03-18 21:00:27 +0100

    Solve eleventh exercise in Foundation

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  x = (x - 32) * (5 / 9);
+  return Math.round(x * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(y) {
+  y = (y * 9 / 5 + 32 );
+  return Math.round(y * 10) / 10;
 };
 
 // Do not edit below this line
