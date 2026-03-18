0056cb1d28826ab7e0ef630d6ca3d1fd41566a33
temen-eng <temen.engineering@gmail.com>
2026-03-18 20:04:07 +0100

    Solve eight exercise in Foundation

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array) {
+    for (let i = 1; i < arguments.length; i++) {
+        array = array.filter(item => item !== arguments[i]);
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
