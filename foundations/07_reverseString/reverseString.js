c48b8c3886d230e0fbaa1b2cacbc7288dd47b223
temen-eng <temen.engineering@gmail.com>
2026-03-18 19:02:32 +0100

    Solve seventh Foundation exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = '';
+    for ( let i = string.length - 1; i >= 0; i-- ) {
+        reversed = reversed + string.charAt(i);
+    }
+    return reversed;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// Take a string and measure it's length.
+// Reverse loop through each of the items, and add them one by one to a new string or array, that can be then returned.
