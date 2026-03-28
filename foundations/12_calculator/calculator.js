604b9317ccc0418a132f04d35c13aa5a316966e9
temen-eng <temen.engineering@gmail.com>
2026-03-28 20:29:20 +0100

    Solve problem 12, calculat

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,50 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(c, d) {
+  return c - d;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+  for (number of arr) {
+    total += number;
+  }	
+  return total;
 };
 
-const multiply = function() {
 
+
+const multiply = function(arr) {
+  let total = 1;
+  for (number of arr) {
+    total *= number;
+  }	
+  return total;
 };
 
-const power = function() {
-	
+
+
+
+const power = function(base, exponent) {
+    return base ** exponent;		
 };
 
-const factorial = function() {
-	
+
+
+
+const factorial = function(num) {
+	let total = 1;
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }	
+  return total;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = {
