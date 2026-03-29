a8e724c913964d7f0d44573dfdfe2cfa9294f0c4
temen-eng <temen.engineering@gmail.com>
2026-03-29 06:51:37 +0200

    Solved exercise 13 from foundation

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // Make it lowerspace, to avoid surprises.
+    str = str.toLowerCase();
+    // Remove all special character and characters, symbols and spaces.
+    str = str.replace(/[\p{P}\p{S}\s]/gu, '');
+    // Create a reversed string.
+    let reversed = str.split('').reverse().join('');
+    if (str === reversed) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
