b9031ba34cded0b251b79d0097982603a7489f1a
temen-eng <temen.engineering@gmail.com>
2026-03-29 08:46:47 +0200

    Solve 16 and last exercise from Foundation

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  const currentYear = new Date().getFullYear();
 
+  return array.reduce((oldest, person) => {
+    const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+    const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+
+    return personAge > oldestAge ? person : oldest;
+  });
 };
 
 // Do not edit below this line
